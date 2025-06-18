a13d58a0606dac81848fd04bbcbe343e3ae63a06
Michael Schmidt <michael.w.schmidt@proton.me>
2025-06-17 23:55:42 -0500

    create simple branching for consumer/producer docker pattern

--- src/main.py
+++ src/main.py
@@ -14,34 +14,12 @@ def handle_incoming_message(message: dict):
 def main():
     cfg = Configuration.from_yaml()
 
-    while True:
-        print("⏳ Checking weather...")
-        try:
-            weather = get_weather(LAT, LON)
-            code = weather["weathercode"]
-            summary = weathercode_map.get(code, "Unknown")
-            print(f"📡 Current weather: {summary} ({code})")
-
-            if is_bad_weather(code):
-                alert = f"Bad weather: {summary} at {weather['time']}"
-                print(f"🚨 Sending alert: {alert}")
-                send_weather_alert(alert)
-            else:
-                print("✅ Weather is fine.")
-
-        except Exception as e:
-            print(f"⚠️ Error: {e}")
-
-        time.sleep(3600)  # Wait an hour before next check
+    role = cfg.app
+    if role == "producer":
+        KafkaProducerApp(cfg).run(lambda: get_weather(cfg), interval_seconds=60)
+    else:
+        KafkaConsumerApp(cfg).run(lambda: push_message("msg", '42', cfg))
 
 
-
-
-def main():
-
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
