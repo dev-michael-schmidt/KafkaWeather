f25bc09174223d292cc1612d4b46f5c431628653
Michael Schmidt <michael.w.schmidt@proton.me>
2025-06-17 23:54:06 -0500

    move this into a function, not a class method

--- src/telegram.py
+++ src/telegram.py
@@ -1,27 +1,22 @@
 import requests
 
-from src import config
-from src.config import Config
+from src.config import Configuration
 
 
-class TelegramNotifier:
-    def __init__(self, chat_id):
-        self.config = Config()
-        self.apis = config.get('apis')
-        self.bot_token = config.get('telegram_bot_token')
-        self.chat_id = chat_id
-        self.api_url = f"https://api.telegram.org/bot{self.apis.get('telegram_bot_token')}"
+def push_message(msg: str, chat_id: str, cfg: Configuration) -> bool:
+    api_url = cfg.apis.telegram_url_template.format(
+        bot_token=cfg.apis.telegram_bot_token)
 
-    def send_message(self, msg: str) -> bool:
-        payload = {
-            "chat_id": self.chat_id,
-            "text": msg,
-            "parse_mode": "Markdown",
-        }
-        try:
-            response = requests.post(f'{self.api_url}/sendMessage', json=payload, timeout=10)
-            response.raise_for_status()
-            return True
-        except requests.RequestException as e:
-            print(f"Error sending message: {e}")
-            return False
+    payload = {
+        "chat_id": chat_id,
+        "text": msg,
+        "parse_mode": "Markdown",
+    }
+
+    try:
+        response = requests.post(f"{api_url}/sendMessage", json=payload, timeout=10)
+        response.raise_for_status()
+        return True
+    except requests.RequestException as e:
+        print(f"Error sending message: {e}")
+        return False
