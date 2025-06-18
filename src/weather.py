f52688aebaa666cccc8322ec76ca1aa20a742c85
Michael Schmidt <michael.w.schmidt@proton.me>
2025-06-17 23:52:00 -0500

    Move producer and consumer into their own class as they should be

--- src/weather.py
+++ src/weather.py
@@ -16,51 +16,23 @@ class Weather:
 
     def get_consumer(self):
 
-        # Create a
-        # Kafka consumer
-        self.consumer = KafkaConsumer(
-            self.app.get('topic'),
-            bootstrap_servers=self.app.get('bootstrap_servers'),
-            auto_offset_reset='earliest',
-            enable_auto_commit=True,
-            group_id='my-group',
-            value_deserializer=lambda m: m.decode('utf-8')  # Decode bytes to string
-        )
-        return self.consumer
 
     def get_producer(self):
 
-       producer = KafkaProducer(
-            bootstrap_servers=self.app.get('bootstrap_servers'),
-            value_serializer=lambda v: v.encode('utf-8')  # Convert string to bytes
-        )
-       topic = self.app.get('topic')
-       message = self.get_weather()
-       producer.send(topic, value=message)
-       producer.flush()
-
-    def get_weather(self):
-
-        name = self.app.get('name', 'Area 51')
-        params = {
-            "latitude": self.location.get('lat', 37.2350),
-            "longitude": self.location.get('long', -115.8111),
-            "current_weather": True,
-            "hourly": "temperature_2m,precipitation",
-            "timezone": "auto",
-        }
+    params = {
+        "latitude": weather_config.location.lat,
+        "longitude": weather_config.location.long,
+        "current_weather": True,
+        "hourly": "temperature_2m,precipitation",
+        "timezone": "auto",
+    }
 
         response = requests.get(self.apis.get('weather_url'), params=params)
         response.raise_for_status()  # Raises error for HTTP failures
         data = response.json()
 
-        weather_code = data['current_weather']['weathercode']
-        if weather_code in rain_codes:
-            return f"it's raining in {name}"
-
-        if weather_code in snow_codes:
-            return f"it's snowing in {name}"
     weather_code = data['current_weather']['weathercode']
+    name = weather_config.location.name
     if weather_code in WeatherCodeMap.rain_codes:
         data['result'] = f"it's raining in {name}"
     elif weather_code in WeatherCodeMap.snow_codes:
